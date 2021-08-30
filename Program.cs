588d5cd33b05dcd02ddb18cd3613be09facbfdda
Jussi Mattila <jussi.mattila@combinostics.com>
2021-08-30 13:55:51 +0300

    Provide repro

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using IronPdf;
+
+namespace IronPDFPublishBug
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"IronPDF is licensed: {License.IsLicensed}");
+            var renderer = new ChromePdfRenderer();
+            var result = renderer.RenderHtmlAsPdf("<h1>Hello multiple entries</h1>");
+            result.SaveAs("hello.pdf");
+        }
+    }
+}
